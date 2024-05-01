11b216b9b18c77490cab43ff5e808698c6160000
Sam McCormack <samvjsmccormack@gmail.com>
2024-05-01 12:35:14 +0100

    switch

--- switch.php
+++ switch.php
@@ -1,5 +1,13 @@
 <?php
 
-header("Location: https://sam-mccormack.co.uk/Test/index.php");
-// test
+$redir = $_GET["redir"];
+
+if ($redir === "index" || $redir === "index.php") {
+    // header("Location: https://sam-mccormack.co.uk/Test/index.php?r=1&from=" . $redir);
+    header("Location: https://sam-mccormack.co.uk/Test/index.php?r=1");
+} else {
+    // header("Location: https://sam-mccormack.co.uk/Test/index.php?r=1&from=" . $redir);
+    header("Location: https://sam-mccormack.co.uk/Test/index.php?r=1");
+}
+
 ?>
