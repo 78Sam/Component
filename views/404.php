53d1dd9b87473c46de1c3799814bf6d956960550
Sam McCormack <samvjsmccormack@gmail.com>
2024-05-02 17:36:25 +0100

    mac

--- views/404.php
+++ views/404.php
@@ -4,8 +4,29 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>404</title>
+
+        <style>
+
+            * {
+                margin: 0;
+                padding: 0;
+            }
+
+            body {
+                width: 100%;
+                height: 100vh;
+
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+            }
+
+        </style>
+
     </head>
     <body>
-        <h1>Unknown page: <?php echo $_SERVER["REQUEST_URI"] ?></h1>
+        <h1>404</h1>
+        <h2>Unknown page: <?php echo $_SERVER["REQUEST_URI"] ?></h2>
     </body>
 </html>
