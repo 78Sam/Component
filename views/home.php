cf7931b979bc491e9b0ac23655d166e27c324411
78Sam <samvjsmccormack@gmail.com>
2024-06-09 16:54:04 +0100

    pc

--- views/home.php
+++ views/home.php
@@ -13,7 +13,7 @@ require_once($REQUIRE_DATABASE);
 
         <base href="/">
 
-        <title>home</title>
+        <title>Home</title>
         <link rel="icon" type="image/x-icon" href="assets/pingu.png">
 
         <link rel="stylesheet" href="styles/main.css">
@@ -31,14 +31,12 @@ require_once($REQUIRE_DATABASE);
             $sections = [
                 _component(name: "section", values: ["title"=>"COMPONENT PHP", "image"=>"test.png", "_id"=>"splash-page"]),
                 _component(
-                    name: "3-col",
+                    name: "short-stack",
                     values: [
                         "image"=>"component.png",
                         "title"=>"Components",
                         "sub-heading"=>"gen component 'name'",
-                        "sec-1"=>"<h2>The Skinny</h2><br><p>Components are the core way to create and organise content that will be visible on your site. Using the above command, a component and its corresponding stylesheet will be created. Components, like every other type of generable element are created based off your schematic within the schematics folder.</p>",
-                        "sec-2"=>"<h2>Values</h2><br><p>Components are able to store placeholder values to be hydrated at runtime. These values are denoted by a wrapping set of @'s, such as @myvalue@.</p>",
-                        "sec-3"=>"<h2>Defaults</h2><br><p>Components themselves are able to use other components by adding a HTML tag that specifies the components name, such as &lt;component-section>&lt;/component-section> these components values are then able to be hydrated at the same time as the calling component.</p>"
+                        "tags"=>"<h3>Reusable</h3><h3>@Values@</h3><h3>&lt;Defaults&gt;</h3>"
                     ]
                 )
             ];
