c85c147b8dd2d42618ef84adb2d6b99fab286d8a
Sam McCormack <samvjsmccormack@gmail.com>
2024-08-18 16:16:59 +0100

    fixed and improved looped components

--- components/component.php
+++ components/component.php
@@ -42,46 +42,69 @@ function component(string $name, array $values=[], bool $echo=true, bool $fill=t
 
     // Looped components
 
-    preg_match_all("/@!{([a-zA-Z0-9]+)}([\s\S]*?)!@/", $html, $loop_element_matches);
+    preg_match_all("/@!{([a-zA-Z0-9]+)}([\s\S]*?)!@/", $html, $looped_section_matches);
 
-    $looped_elements = [];
-    for ($i = 0; $i < count($loop_element_matches[1]); $i++) {
+    // Component -> Looped Section -> Looped Element
 
-        $val_to_count = $values[$loop_element_matches[1][$i]] ?? [];
+    for ($looped_section_index = 0; $looped_section_index < count($looped_section_matches[1]); $looped_section_index++) {
+
+        // Check if there are values present for the looped component
+
+        $val_to_count = $values[$looped_section_matches[1][$looped_section_index]] ?? [];
+
+        // Check its an array that we can loop through and not just some constant
+
         if (!is_array($val_to_count)) {
             $val_to_count = [];
         }
 
-        $looped_elements[] = str_repeat($loop_element_matches[2][$i], count($val_to_count));
-        preg_match_all("/{i}/", $looped_elements[$i], $index_matches);
+        $num_sections = count($val_to_count);
 
-        $count = 1;
-        foreach ($index_matches[0] as $counter) {
-            $index = strpos($looped_elements[$i], $counter);
-            if ($index !== false) {
-                $looped_elements[$i] = substr_replace($looped_elements[$i], $count, $index, strlen($counter));
-            }
-            $count += 1;
+        $looped_section_name = $looped_section_matches[1][$looped_section_index];
+        $looped_sections_hydrated = "";
+
+        // Get the looped elements of the section (looped section without @!{rows} ... !@)
+
+        for ($i = 0; $i < $num_sections; $i++) {
+            $looped_elements[] = $looped_section_matches[2][$looped_section_index];
         }
 
-        $html = str_replace($loop_element_matches[0][$i], $looped_elements[$i], $html);
+        for ($looped_element_index = 0; $looped_element_index < $num_sections; $looped_element_index++) {
 
-    }
+            // Get all the values in the looped element
 
-    $adds = [];
-    foreach ($values as $val) {
-        if (is_array($val)) {
-            $i = 1;
-            foreach ($val as $section) {
-                foreach ($section as $key => $value) {
-                    $adds["{$key}-{$i}"] = $value;
-                    $i++;
+            preg_match_all("/@[^@]*@/", $looped_elements[$looped_element_index], $looped_element_matches);
+
+            // Hydrate the looped element values
+
+            foreach ($looped_element_matches[0] as $val) {
+
+                $val = trim($val, "@");
+
+                // Do we have a value to replace the placeholder
+
+                if (isset($values[$looped_section_name][$looped_element_index][$val])) {
+
+                    // Replace the placeholder in the looped element
+
+                    $hydration_value = $values[$looped_section_name][$looped_element_index][$val];
+                    $looped_elements[$looped_element_index] = str_replace(
+                        "@{$val}@",
+                        $hydration_value,
+                        $looped_elements[$looped_element_index]
+                    );
+
                 }
-            }
-        }
-    }
 
-    $values = array_merge($values, $adds);
+            }
+
+            $looped_sections_hydrated .= $looped_elements[$looped_element_index];
+
+        }
+
+        $html = str_replace($looped_section_matches[0][$looped_section_index], $looped_sections_hydrated, $html);
+
+    }
 
     // Values
 
