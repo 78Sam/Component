0e9e6af8291398e62fc4b3032f9aa8e0e955c702
Sam McCormack <samvjsmccormack@gmail.com>
2024-08-18 17:25:44 +0100

    Fixing looped components due to issues with @ in values

--- components/component.php
+++ components/component.php
@@ -45,65 +45,72 @@ function component(string $name, array $values=[], bool $echo=true, bool $fill=t
     preg_match_all("/@!{([a-zA-Z0-9]+)}([\s\S]*?)!@/", $html, $looped_section_matches);
 
     // Component -> Looped Section -> Looped Element
-
+    
     for ($looped_section_index = 0; $looped_section_index < count($looped_section_matches[1]); $looped_section_index++) {
-
+    
         // Check if there are values present for the looped component
-
+    
         $val_to_count = $values[$looped_section_matches[1][$looped_section_index]] ?? [];
-
+    
         // Check its an array that we can loop through and not just some constant
-
+    
         if (!is_array($val_to_count)) {
             $val_to_count = [];
         }
-
+    
         $num_sections = count($val_to_count);
-
+    
         $looped_section_name = $looped_section_matches[1][$looped_section_index];
         $looped_sections_hydrated = "";
-
+    
         // Get the looped elements of the section (looped section without @!{rows} ... !@)
-
+    
         for ($i = 0; $i < $num_sections; $i++) {
             $looped_elements[] = $looped_section_matches[2][$looped_section_index];
         }
-
+    
         for ($looped_element_index = 0; $looped_element_index < $num_sections; $looped_element_index++) {
-
+    
             // Get all the values in the looped element
-
+    
             preg_match_all("/@[^@]*@/", $looped_elements[$looped_element_index], $looped_element_matches);
-
+    
             // Hydrate the looped element values
-
-            foreach ($looped_element_matches[0] as $val) {
-
-                $val = trim($val, "@");
-
-                // Do we have a value to replace the placeholder
-
-                if (isset($values[$looped_section_name][$looped_element_index][$val])) {
-
-                    // Replace the placeholder in the looped element
-
-                    $hydration_value = $values[$looped_section_name][$looped_element_index][$val];
-                    $looped_elements[$looped_element_index] = str_replace(
-                        "@{$val}@",
-                        $hydration_value,
-                        $looped_elements[$looped_element_index]
-                    );
-
+    
+            if ($fill) {
+    
+                foreach ($looped_element_matches[0] as $val) {
+    
+                    $val = trim($val, "@");
+        
+                    // Do we have a value to replace the placeholder
+        
+                    if (isset($values[$looped_section_name][$looped_element_index][$val])) {
+        
+                        // Replace the placeholder in the looped element
+        
+                        $hydration_value = $values[$looped_section_name][$looped_element_index][$val];
+        
+                        $hydration_value = str_replace("@", "{_temp_at_sign}", $hydration_value);
+        
+                        $looped_elements[$looped_element_index] = str_replace(
+                            "@{$val}@",
+                            $hydration_value,
+                            $looped_elements[$looped_element_index]
+                        );
+        
+                    }
+        
                 }
-
+    
             }
-
+    
             $looped_sections_hydrated .= $looped_elements[$looped_element_index];
-
+    
         }
-
+    
         $html = str_replace($looped_section_matches[0][$looped_section_index], $looped_sections_hydrated, $html);
-
+    
     }
 
     // Values
@@ -115,6 +122,8 @@ function component(string $name, array $values=[], bool $echo=true, bool $fill=t
             $html = str_replace("@{$val}@", $values[$val] ?? "", $html);
         }
     }
+
+    $html = str_replace("{_temp_at_sign}", "@", $html);
 
     if ($echo) {
         echo $html;
